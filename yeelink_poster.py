bbd98086e1cd3e24af8cbd7d3c18dc6af212829f
yuehuTi <yuehu_p@hotmail.com>
2015-12-30 23:45:40 +0800

    add:running log

--- yeelink_poster.py
+++ yeelink_poster.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*- 
 import os
+import sys
 import imp
 import time
 import requests
@@ -11,12 +12,20 @@ api_headers={'U-ApiKey':api_key,'content-type': 'application/json'}
 
 sensors = []
 
+def logout(str):
+    print "[%s]%s"%(time.strftime("%Y-%m-%d %H:%M:%S"), str)
+    sys.stdout.flush()
+
+
 def upload_data_to_yeelink(device, sensor, data):
     url=r'%s/device/%s/sensor/%s/datapoints' % (api_url,device,sensor)
     strftime=time.strftime("%Y-%m-%dT%H:%M:%S")
     data={"timestamp":strftime , "value": data}
     res=requests.post(url,headers=api_headers,data=json.dumps(data))
-
+    if res.status_code != 200:
+        return (False, res.status_code)
+    else:
+        return (True, None)
 
 def load_sensors():
     sensor_path = os.path.abspath("Sensor") + "/"
@@ -32,20 +41,22 @@ def load_sensors():
             instance = class_obj()
             sensors.append(instance)
         except Exception, e:
-            print "Sensor %s load error:%s"%(filename, e)
+            logout("Sensor %s load error:%s"%(filename, e)) 
 
 def run():
     while True:
         for sensor in sensors:
             try:
+                logout("Sensor %s fetching data..."%sensor.__class__.__name__) 
                 result = sensor.GetData()
                 for data in result:
-                    upload_data_to_yeelink(data["device"], data["sensor"], data["data"])
+                    res, err = upload_data_to_yeelink(data["device"], data["sensor"], data["data"])
+                    logout("SensorID: %s Data: %s Uploaed: %s"%(str(data["sensor"]), str(data["data"]), "Success" if res else "Failed: %s"%err)) 
             except Exception, e:
-                print str(e)
+                logout(str(e))
+        logout( "Sleep")
         time.sleep(1*60)
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     load_sensors()
     run()
-        
